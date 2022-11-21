7b54d5faa69ffc89494d7813d0748d8747e6e94b
Mike Oram <Mikeoram01@gmail.com>
2022-11-21 16:03:05 +0000

    Added company dropdown as spec

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const navBarOpen = document.getElementById('nav-bar-menu-open');
 const navBarClose = document.getElementById('nav-bar-menu-close');
 const openFeatures = document.getElementById('open-features-menu');
+const openCompanies = document.getElementById('open-company-menu');
 const arrowDownFeatures = document.getElementById('arrow-down-features');
-const arrowUpFeatures = document.getElementById('arrow-up-features')
+const arrowDownCompany = document.getElementById('arrow-down-company');
+const arrowUpFeatures = document.getElementById('arrow-up-features');
+const arrowUpCompany = document.getElementById('arrow-up-company');
 const featuresDropdown = document.getElementById('features-dropdown');
+const companyDropdown = document.getElementById('company-dropdown');
 const navBarContainer = document.getElementById('nav-bar-container');
 const overlay = document.getElementById('overlay');
 
@@ -20,6 +24,14 @@ const handleFeaturesClick = () => {
     featuresDropdown.classList.toggle('hidden');
 }
 
+const handleCompanyClick = () => {
+    arrowDownCompany.classList.toggle('hidden');
+    arrowUpCompany.classList.toggle('hidden');
+    companyDropdown.classList.toggle('hidden');
+
+}
+
 navBarOpen.addEventListener('click', handleMenu);
 navBarClose.addEventListener('click', handleMenu);
 openFeatures.addEventListener('click', handleFeaturesClick);
+openCompanies.addEventListener('click', handleCompanyClick);
