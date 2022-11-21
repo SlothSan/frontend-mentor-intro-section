8b9637e42474a21d7742a134f02e87789f8072ef
Mike Oram <Mikeoram01@gmail.com>
2022-11-21 13:49:32 +0000

    Added hamburge menu functionality

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const navBarOpen = document.getElementById('nav-bar-menu-open');
+const navBarClose = document.getElementById('nav-bar-menu-close');
+const navBarContainer = document.getElementById('nav-bar-container');
 
-const handleMenuOpen = () => {
-    console.log('Hello!')
+const handleMenu = () => {
+    navBarOpen.classList.toggle('hidden');
+    navBarClose.classList.toggle('hidden');
+    navBarContainer.classList.toggle('hidden');
 }
 
-navBarOpen.addEventListener('click', handleMenuOpen);
+navBarOpen.addEventListener('click', handleMenu);
+navBarClose.addEventListener('click', handleMenu);
