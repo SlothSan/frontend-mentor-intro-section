1eabd1c8503addcc2af488ea52880ffa7f12b49e
Mike Oram <Mikeoram01@gmail.com>
2022-11-21 15:01:34 +0000

    Added overlay on menu click

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const navBarOpen = document.getElementById('nav-bar-menu-open');
 const navBarClose = document.getElementById('nav-bar-menu-close');
 const navBarContainer = document.getElementById('nav-bar-container');
+const overlay = document.getElementById('overlay');
 
 const handleMenu = () => {
+    overlay.classList.toggle('hidden');
     navBarOpen.classList.toggle('hidden');
     navBarClose.classList.toggle('hidden');
     navBarContainer.classList.toggle('hidden');
