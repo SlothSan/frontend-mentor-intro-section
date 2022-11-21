a0742717fece3865880a29487a3db6c16b56c368
Mike Oram <Mikeoram01@gmail.com>
2022-11-21 15:48:46 +0000

    Added dropdown for features sub menu

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const navBarOpen = document.getElementById('nav-bar-menu-open');
 const navBarClose = document.getElementById('nav-bar-menu-close');
+const openFeatures = document.getElementById('open-features-menu');
+const arrowDownFeatures = document.getElementById('arrow-down-features');
+const arrowUpFeatures = document.getElementById('arrow-up-features')
+const featuresDropdown = document.getElementById('features-dropdown');
 const navBarContainer = document.getElementById('nav-bar-container');
 const overlay = document.getElementById('overlay');
 
@@ -10,5 +14,12 @@ const handleMenu = () => {
     navBarContainer.classList.toggle('hidden');
 }
 
+const handleFeaturesClick = () => {
+    arrowDownFeatures.classList.toggle('hidden');
+    arrowUpFeatures.classList.toggle('hidden');
+    featuresDropdown.classList.toggle('hidden');
+}
+
 navBarOpen.addEventListener('click', handleMenu);
 navBarClose.addEventListener('click', handleMenu);
+openFeatures.addEventListener('click', handleFeaturesClick);
